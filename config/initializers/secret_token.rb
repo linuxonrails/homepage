7960452c45e22a9425f280b5b3d449811d704428
Linux on Rails <linuxonrails@linuxonrails.com>
2010-11-14 20:31:14 +0100

    Initial push (rails sekeleton)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homepage::Application.config.secret_token = 'bd3cee1d27246f181664d106c65a306f2a53a0170aa0a975ba66babbae2bdc5999d786de42a09369c73a12e18f49f9fa73c0071846bfc03ad4918d56ee344a76'
